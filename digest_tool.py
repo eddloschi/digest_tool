78a8fa1568d62535a691ce57fd77dc5cfd1dde69
Maurício Batista <eddloschi@gmail.com>
2013-01-27 22:46:08 -0200

    Hash function implemented

--- digest_tool.py
+++ digest_tool.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import hashlib
+from flask import Flask, abort, request
 
 # configuration
 DEBUG = True
@@ -6,6 +7,16 @@ SECRET_KEY = '4bf5efc0-dc19-4128-b040-d74c77bc1b31'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+
+@app.route('/hash/<algorithm>', methods=['POST'])
+def hash_message(algorithm):
+    try:
+        h = hashlib.new(algorithm)
+    except ValueError:
+        abort(404)
+    h.update(request.form['message'])
+    return h.hexdigest()
 
 if __name__ == '__main__':
     app.run()
