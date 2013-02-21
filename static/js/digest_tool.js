3d585b7cc4d57915bc0c3954a13364f6421014a0
Maurício Batista <eddloschi@gmail.com>
2013-02-21 05:25:02 -0300

    Loading indicator added

--- static/js/digest_tool.js
+++ static/js/digest_tool.js
@@ -4,21 +4,25 @@ var request_digest = function (algorithm) {
     if (requests[algorithm] && requests[algorithm].readyState < 4) {
         requests[algorithm].abort();
     }
+    var id = '#'+algorithm;
     requests[algorithm] = $.post('/hash/'+algorithm, { message: $('#message').val() }, function (data) {
-        id = '#'+algorithm;
         if ($(id).hasClass('text-error')) {
             $(id).removeClass('text-error');
         }
         $(id).text(data);
     }).fail(function () {
         if (requests[algorithm].statusText !== 'abort') {
-            id = '#'+algorithm;
             if (!$(id).hasClass('text-error')) {
                 $(id).addClass('text-error');
             }
             $(id).text('ERROR RETRIEVING RESULT');
         }
+    }).always(function () {
+        $('#loading'+algorithm).hide();
+        $(id).show();
     });
+    $(id).hide();
+    $('#loading'+algorithm).show();
 };
 
 $(document).ready(function () {
